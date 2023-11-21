73ea3661c8ba3514e0d19671568910c906ee1380
MarcosV22 <149092855+MarcosV22@users.noreply.github.com>
2023-11-20 20:35:23 -0400

    main1.c

--- Atividade 16112023/Recursividade (2023111601)/main1.c
+++ Atividade 16112023/Recursividade (2023111601)/main1.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#include "vetor.h"
+#include <math.h>
+
+int k_esimo_digito(int n, int k) {
+
+    if (k <= 0) {
+        return -1;
+    }
+
+
+    if (k == 1) {
+        return n % 10;
+
+    } else {
+
+        return k_esimo_digito(n / 10, k - 1);
+    }
+}
 
 int main() {
-    Vetor v1 = {1.0, 2.0, 3.0};
-    Vetor v2 = {4.0, 5.0, 6.0};
-    Vetor resultado_soma;
 
-    // Realiza a soma entre v1 e v2 e armazena o resultado em resultado_soma
-    soma(&v1, &v2, &resultado_soma);
 
-    // Imprime o resultado da soma
-    printf("Soma: (%.2f, %.2f, %.2f)\n", resultado_soma.x, resultado_soma.y, resultado_soma.z);
+    int resultado = k_esimo_digito(2845, 2);
 
-    // Calcula e imprime o produto escalar entre v1 e v2
-    float resultado_produto_escalar = produto_escalar(&v1, &v2);
-    printf("Produto Escalar: %.2f\n", resultado_produto_escalar);
+    if (resultado != -1) {
+        printf("O kesimo digito eh: %d\n", resultado);
+    }
 
     return 0;
 }
